85c8022648be30f5dff485fe6f742763b6e4ca5f
banti377 <patelbanti377@yahoo.in>
2021-11-01 21:03:32 +0530

    initial state added for redux

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,13 @@
 import { Provider } from 'react-redux';
 import { store } from '../state';
+import RepositoriesList from './RepositoriesList';
 
 const App = () => {
   return (
     <Provider store={store}>
       <div>
         <h1>Search a Package</h1>
+        <RepositoriesList />
       </div>
     </Provider>
   );
